9a3ac250c900322b165324f5741c32f3e11a58a1
Adrian Balan <adrianbalan1996@icloud.com>
2023-03-23 16:52:09 +0200

    implemented the users function

--- src/sys/sys.h
+++ src/sys/sys.h
@@ -1,29 +1,35 @@
 #ifndef ASMOT_SYS_H
 #define ASMOT_SYS_H
 
+#include <limits.h>
 #include <sys/utsname.h>
+#include <utmp.h>
 
 #include "../types.h"
-#define SYS_BUFLEN 64
+#define SYSINFO_BUFLEN 64
 
 typedef struct sysinfo_s {
-    char kernel[_UTSNAME_LENGTH];   /* kernel name */
-    char release[_UTSNAME_LENGTH];  /* kernel release */
-    char version[_UTSNAME_LENGTH];  /* kernel version */
-    char hostname[_UTSNAME_LENGTH]; /* nodename */
-    char hw_type[_UTSNAME_LENGTH];  /* hardware type */
-    char chasis_vendor[SYS_BUFLEN]; /* chassis vendor */
-    char vendor[SYS_BUFLEN];        /* system vendor */
-    char product_name[SYS_BUFLEN];  /* product name */
+    char kernel[_UTSNAME_LENGTH];       /* kernel name */
+    char release[_UTSNAME_LENGTH];      /* kernel release */
+    char version[_UTSNAME_LENGTH];      /* kernel version */
+    char hostname[_UTSNAME_LENGTH];     /* nodename */
+    char hw_type[_UTSNAME_LENGTH];      /* hardware type */
+    char chasis_vendor[SYSINFO_BUFLEN]; /* chassis vendor */
+    char vendor[SYSINFO_BUFLEN];        /* system vendor */
+    char product_name[SYSINFO_BUFLEN];  /* product name */
 } sysinfo_t;
 
-typedef struct user_s {
-} user_t;
+typedef struct suser_s {
+    char name[UT_NAMESIZE];
+    char terminal[UT_LINESIZE];
+    char host[UT_HOSTSIZE];
+    u64 started;
+    pid_t pid;
+} suser_t;
 
 // TODO: Add documentation
 i32 sys_info(sysinfo_t* sys);
 u64 boot_time(void);
-// TODO: Implement
-i32 users(user_t* users);
+i32 users(suser_t* users, u32 n_users);
 
 #endif  // ASMOT_SYS_H
--- src/sys/users.c
+++ src/sys/users.c
@@ -1,4 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <utmp.h>
+
 #include "sys.h"
 
-int users(user_t* users) {
+i32 users(suser_t* users, u32 n_users) {
+    struct utmp* entry;
+    suser_t* user;
+    u32 count = 0;
+    while ((entry = getutent()) != NULL) {
+        if (count >= n_users) {
+            // TODO:  Add error handling for this
+            break;
+        }
+
+        if (entry->ut_type == USER_PROCESS) {
+            user = &users[count++];
+            memcpy(user->name, entry->ut_user, UT_NAMESIZE);     /* username */
+            memcpy(user->terminal, entry->ut_line, UT_LINESIZE); /* terminal name suffix */
+            memcpy(user->host, entry->ut_host, UT_HOSTSIZE);     /* hostname for login */
+            user->pid = entry->ut_pid;                           /* PID of the login process */
+            user->started = entry->ut_tv.tv_sec;                 /* time entry was made in seconds */
+        }
+    }
+
+    endutent();
+    return count;
 }
